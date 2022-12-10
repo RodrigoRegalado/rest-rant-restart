323a97d3d4a70aaa7f660355b5300404411d0ce5
Rodrigo Regalado <r.regalado6204@gmail.com>
2022-12-10 13:56:32 -0800

    quick save, still doesnt work

--- controllers/places.js
+++ controllers/places.js
@@ -1,6 +1,12 @@
 const router = require('express').Router()
 const places = require('../models/places.js')
 
+
+router.get('/', (req, res) => {
+  res.render('places/index', { places })
+})
+
+
 router.get('/new', (req, res) => {
   res.render('places/new')
 })
@@ -14,7 +20,21 @@ router.get('/:id', (req, res) => {
     res.render('error404')
   }
   else {
-    res.render('places/show', {place: places[id] })
+    res.render('places/show', { place: places[id], id })
+  }
+})
+
+router.delete('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    places.splice(id, 1)
+    res.redirect('/places')
   }
 })
 
@@ -30,10 +50,6 @@ router.post('/', (req, res) => {
   }
   places.push(req.body)
   res.redirect('/places')
-})
-
-router.get('/', (req, res) => {
-    res.render('places/index', { places })
 })
 
 module.exports = router 
